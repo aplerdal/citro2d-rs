e8b9a742bf5bcc21a1380809ac2438e52b60c698
Antimattur <59904070+aplerdal@users.noreply.github.com>
2023-09-08 10:34:52 -0500

    Write font.rs
    Noting technically required from this so I think I can probably remove it later. Keeping it temp for simplicity

--- citro2d-sys/src/font.rs
+++ citro2d-sys/src/font.rs
@@ -1,1 +1,15 @@
+//! Definitions from `<c2d/font.h>`
 
+use core::ptr::null;
+
+use crate::*;
+
+
+/*
+internal function so doesn't matter
+
+#[inline]
+pub static fn C2Di_FontAlloc() -> C2D_Font {
+  return (*mut C2D_Font_s {_unused: {}}) as C2D_Font;
+}
+*/
