44457b97693cb44524304a1bd57ff9157763e4cb
aplerdal <aplerdal01@gmail.com>
2023-09-08 16:19:06 -0500

    Fix error

--- citro2d-sys/src/sprite.rs
+++ citro2d-sys/src/sprite.rs
@@ -4,12 +4,22 @@ use core::ptr::null;
 
 use crate::*;
 
+pub const M_PI : f32 = 3.14159265358979323846;
+pub const M_TAU : f32 = 2.0_f32*M_PI;
+/// ### C3D_AngleFromDegrees
+/// converts radians to degrees\
+/// **params:**\
+/// `_angle` - Angle in degrees
+pub fn C3D_AngleFromDegrees(_angle : f32) -> f32 {
+    return (_angle*M_TAU);
+}
+
 #[inline]
-pub static fn C2D_SpriteFromImage(sprite: &mut C2D_Sprite, image: C2D_Image){
+pub unsafe fn C2D_SpriteFromImage(sprite: &mut C2D_Sprite, image: C2D_Image){
     sprite.image           = image;
     sprite.params.pos.x    = 0.0_f32;
     sprite.params.pos.y    = 0.0_f32;
-    sprite.params.pos.w.   = image.subtex.width;
+    sprite.params.pos.w    = image.subtex.width;
     sprite.params.pos.h    = image.subtex.height;
     sprite.params.center.x = 0.0_f32;
     sprite.params.center.y = 0.0_f32;
@@ -17,23 +27,23 @@ pub static fn C2D_SpriteFromImage(sprite: &mut C2D_Sprite, image: C2D_Image){
     sprite.params.depth    = 0.0_f32;
 }
 #[inline]
-pub static fn C2D_SpriteFromSheet(sprite: &mut C2D_Sprite, sheet: C2D_SpriteSheet ,index:u32) {
+pub unsafe fn C2D_SpriteFromSheet(sprite: &mut C2D_Sprite, sheet: C2D_SpriteSheet ,index:u32) {
      C2D_SpriteFromImage(sprite, C2D_SpriteSheetGetImage(sheet, index));
 }
 
 #[inline]
-pub static fn C2D_SpriteScale(sprite: &mut C2D_Sprite, x:f32, y:f32) {
+pub unsafe fn C2D_SpriteScale(sprite: &mut C2D_Sprite, x:f32, y:f32) {
     sprite.params.pos.w *= x;
     sprite.params.pos.h *= y;
     sprite.params.center.x *= x;
     sprite.params.center.y *= y;
 }
 #[inline]
-pub static fn C2D_SpriteRotate(sprite: &mut C2D_Sprite, radians:f32) {
+pub unsafe fn C2D_SpriteRotate(sprite: &mut C2D_Sprite, radians:f32) {
     sprite.params.angle += radians;
 }
 #[inline]
-pub static fn C2D_SpriteRotateDegrees(sprite: &mut C2D_Sprite, degrees:f32) {
+pub unsafe fn C2D_SpriteRotateDegrees(sprite: &mut C2D_Sprite, degrees:f32) {
     C2D_SpriteRotate(sprite, C3D_AngleFromDegrees(degrees));
 }
 
