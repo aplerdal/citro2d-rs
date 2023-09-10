c65b2de5fc765a3f7b409e15c0864027a4d3a6b8
aplerdal <aplerdal01@gmail.com>
2023-09-09 20:51:05 -0500

    Change build.rs

--- citro2d-sys/build.rs
+++ citro2d-sys/build.rs
@@ -2,14 +2,13 @@ use std::env;
 
 fn main() {
     let dkp_path = env::var("DEVKITPRO").unwrap();
-    let debug_symbols = env::var("DEBUG").unwrap();
-
+    let debug_symbols = "0";
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=DEVKITPRO");
     println!("cargo:rustc-link-search=native={dkp_path}/libctru/lib");
     println!(
         "cargo:rustc-link-lib=static={}",
-        match debug_symbols.as_str() {
+        match debug_symbols {
             // Based on valid values described in
             // https://doc.rust-lang.org/cargo/reference/profiles.html#debug
             "0" | "false" => "citro2d",
